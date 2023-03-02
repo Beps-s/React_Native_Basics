f5daf930e8f4cb0f3cdfa85d0fd30bcf7243f7ff
UwUBeebi <chris.erman9@gmail.com>
2023-03-02 15:12:37 +0200

    Create homepage screen

--- src/screens/app/Home/styles.js
+++ src/screens/app/Home/styles.js
@@ -3,6 +3,9 @@ import { colors } from "../../../utils/colors";
 
 export const styles = StyleSheet.create({
     container: {
-
+        padding: 16
+    },
+    list: {
+        paddingVertical: 24
     }
 })
